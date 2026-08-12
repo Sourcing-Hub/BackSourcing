baf6a09f0a72f3841cb9786a783dd7e61edf4cfd
Administrateur <admin@sourcinghub.com>
2026-08-12 17:03:14 +0000

    feat: configuration des routes pour formulaires

---
+++ formulaires/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from .views import (
+    FormulaireListeView, FormulaireDetailView,
+    FormulairePublierView, FormulaireDepublierView,
+    FormulaireAssocierCampagneView, FormulaireReorganiserChampsView,
+    FormulairePrevisualisationView,
+    ChampListeView, ChampDetailView,
+    OptionListeView, OptionDetailView,
+)
+
+urlpatterns = [
+    # Formulaires
+    path('', FormulaireListeView.as_view(), name='formulaires-liste'),
+    path('<uuid:pk>/', FormulaireDetailView.as_view(), name='formulaires-detail'),
+    path('<uuid:pk>/publier/', FormulairePublierView.as_view(), name='formulaires-publier'),
+    path('<uuid:pk>/depublier/', FormulaireDepublierView.as_view(), name='formulaires-depublier'),
+    path('<uuid:pk>/associer-campagne/', FormulaireAssocierCampagneView.as_view(), name='formulaires-associer'),
+    path('<uuid:pk>/reorganiser-champs/', FormulaireReorganiserChampsView.as_view(), name='formulaires-reorganiser'),
+    path('<uuid:pk>/previsualiser/', FormulairePrevisualisationView.as_view(), name='formulaires-previsualiser'),
+
+    # Champs
+    path('<uuid:pk>/champs/', ChampListeView.as_view(), name='champs-liste'),
+    path('champs/<uuid:champ_id>/', ChampDetailView.as_view(), name='champs-detail'),
+
+    # Options
+    path('champs/<uuid:champ_id>/options/', OptionListeView.as_view(), name='options-liste'),
+    path('options/<uuid:option_id>/', OptionDetailView.as_view(), name='options-detail'),
+]
