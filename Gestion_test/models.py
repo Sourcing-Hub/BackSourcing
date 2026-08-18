0a7d4312e6971ee0dd89439481216b7d38125f69
Kya127 <cheikhamariamba@gmail.com>
2026-08-18 18:52:49 +0200

    feat(backend): ajout du modele Test  pour la création de test

---
+++ Gestion_test/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Test(models.Model):
+    class StatusChoices(models.TextChoices):
+        ACTIF = 'actif', 'Actif'
+        INACTIF = 'inactif', 'Inactif'
+        NOUVEAU = 'nouveau', 'Nouveau'
+
+    nom = models.CharField(max_length=255, verbose_name="Nom du test")
+   
+    campagne_assossiée = models.CharField(max_length=255, verbose_name="Formation/Offre associée", blank=True, null=True)
+    
+    lien_ressource = models.URLField(max_length=500, blank=True, null=True, verbose_name="Lien de la ressource (URL)")
+    
+    fichier_ressource = models.FileField(upload_to='tests_resources/', blank=True, null=True, verbose_name="Fichier de ressource")
+    
+    description = models.TextField(verbose_name="Description / Consignes")
+    
+    statut = models.CharField(
+        max_length=20, 
+        choices=StatusChoices.choices, 
+        default=StatusChoices.NOUVEAU, 
+        verbose_name="Statut"
+    )
+    
+    date_creation = models.DateTimeField(auto_now_add=True, verbose_name="Date de création")
+
+    def __str__(self):
+        return f"{self.nom} ({self.get_statut_display()})"
