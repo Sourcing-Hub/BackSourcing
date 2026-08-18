5e97f41112f5d35bbe1837c67acd375fbddc9453
Kya127 <cheikhamariamba@gmail.com>
2026-08-18 19:45:05 +0200

    feat(backend): creation du serializer pour la soumission de test

--- Gestion_test/serializers.py
+++ Gestion_test/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Test
+from .models import SoumissionTest
 
 class TestSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +9,11 @@ class TestSerializer(serializers.ModelSerializer):
         
     # Optionnel : Tu pourras ajouter ici des validations personnalisées plus tard
     # par exemple, vérifier la taille du fichier ou le format de l'URL
+
+   
+
+class SoumissionTestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SoumissionTest
+        fields = ['id', 'nom_candidat', 'email_candidat', 'fichier_test', 'date_soumission']
+        read_only_fields = ['id', 'date_soumission']
